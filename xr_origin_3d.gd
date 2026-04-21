2efc8b1ab8e60879dd1b2ebe924fb40c437dc7a2
haraldvr123 <harald.toft@icloud.com>
2026-04-22 00:51:24 +0200

    fixed a bug in the flying script

--- xr_origin_3d.gd
+++ xr_origin_3d.gd
@@ -7,27 +7,23 @@ extends XROrigin3D
 var speed = 3.0   # movement speed (meters per second)
  
 func _physics_process(delta):
-	# 1. Get joystick input (Vector2)
+	# Get joystick input (Vector2)
 	var input_vec = left_controller.get_vector2("primary")
  
-	# 2. Ignore tiny input (deadzone)
-	if input_vec.length() < 0.1:
-		return
- 
-	# 3. Get direction based on where the headset is facing
+	# Get direction based on where the headset is facing
 	var forward = -camera.global_transform.basis.z
 	var right = camera.global_transform.basis.x
  
-	# 4. Prevent vertical movement
+	# Prevent vertical movement
 	forward.y = 0
 	right.y = 0
 	forward = forward.normalized()
 	right = right.normalized()
  
-	# 5. Combine joystick input with directions
+	# Combine joystick input with directions
 	var direction = forward * input_vec.y + right * input_vec.x
  
-	# 6. Move the player (XROrigin3D)
+	# Move the player (XROrigin3D)
 	global_position += direction * speed * delta
  
 	var turn_input = right_controller.get_vector2("primary").x
@@ -36,14 +32,6 @@ func _physics_process(delta):
 		
 	# implements flying
 	var vertical = 0.0
-	
-	if left_controller.is_button_pressed("ax_touch"):
-		print("go up")
-		vertical += 1.0
-	
-	if left_controller.is_button_pressed("by_touch"):
-		print("go down")
-		vertical += -1.0
 	
 	if 	right_controller.is_button_pressed("ax_touch"):
 		print("go up")
