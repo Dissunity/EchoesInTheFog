acf5826a7b2446cc9389c8bc22c0da3ff9d337a8
Marin Prusac <prusac.marin@gmail.com>
2026-04-22 18:24:18 +0200

    separated debug logic and added methods for opening and closing of journal, functionality can be tested in the demo scene

--- models/present/journal/journal.gd
+++ models/present/journal/journal.gd
@@ -1,9 +1,32 @@
 extends Node3D
 
+signal opened
+signal closed
+
+var is_open = false
+@export var debug = false
 
 
 func _process(_delta: float) -> void:
-	if Input.is_action_just_pressed("ui_accept"):
-		$AnimationPlayer.play("ArmatureAction")
-		await get_tree().create_timer(3).timeout
-		$AnimationPlayer.play_backwards("ArmatureAction")
+	if debug and Input.is_action_just_pressed("ui_accept"):
+		toggle_open()
+
+func toggle_open():
+	if is_open:
+		close_journal()
+	else:
+		open_journal()
+
+func open_journal():
+	if is_open:
+		return
+	$AnimationPlayer.play("ArmatureAction")
+	is_open = true
+	opened.emit()
+
+func close_journal():
+	if not is_open:
+		return
+	$AnimationPlayer.play_backwards("ArmatureAction")
+	is_open = false
+	closed.emit()
