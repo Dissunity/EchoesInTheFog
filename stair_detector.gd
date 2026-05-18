0de4a270c6514b091f4cce1a8220ad8dd440e863
Marin Prusac <prusac.marin@gmail.com>
2026-05-18 17:50:48 +0200

    added stair climbing, shortened the player

---
+++ stair_detector.gd
@@ -0,0 +1,14 @@
+extends RayCast3D
+class_name StairDetector
+
+func is_detecting_stairs() -> bool:
+	var collider = get_collider() as Node3D
+	if not collider:
+		return false
+	var collider_parent = collider.get_parent()
+	if not collider_parent:
+		return false
+	if collider_parent.name != "Stairs_001":
+		return false
+	return true
+	
