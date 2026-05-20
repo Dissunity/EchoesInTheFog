0d6db9200b36798db0cff7c463f040ccba7fab8b
charmaine211 <charmaine211@hotmail.com>
2026-05-21 01:15:51 +0200

    Basic monster spawning is implemented. Still need to test the whole game follow.

--- mechanics/game_manager/game_manager.gd
+++ mechanics/game_manager/game_manager.gd
@@ -3,6 +3,7 @@ extends Node3D
 @export var player: XROrigin3D
 @export var puzzleHolder: Area3D
 @export var monster: Node3D
+@export var vr_fade: Node3D 
 
 # To see whether the player is in the fresnel room
 # And the puzzle piece is on the snap area
@@ -28,6 +29,7 @@ func _check_conditions():
 		if player_is_high_enough:
 			print("Monster should appear")
 			monster.spawn()
+			_on_game_end()
 
 func _on_player_height_changed(is_high_enough: bool):
 	player_is_high_enough = is_high_enough
@@ -38,3 +40,20 @@ func _on_puzzle_piece_snapped() -> void:
 	puzzle_is_snapped = true
 	print("GameManager puzzleholder status changes to: ", puzzle_is_snapped)
 	_check_conditions()
+	
+func _on_game_end() -> void:
+	await get_tree().create_timer(3.0).timeout
+	if vr_fade and vr_fade.has_method("fade_to_black"):
+		vr_fade.fade_to_black(2.0) 
+		
+		await get_tree().create_timer(2.0).timeout
+	else:
+
+		var fade_tween = create_tween()
+		fade_tween.tween_property(vr_fade, "material_override:albedo_color:a", 1.0, 2.0)
+		await fade_tween.finished
+		
+	await get_tree().create_timer(1.0).timeout
+	
+	print("Game ended. VR Application closing...")
+	get_tree().quit()
