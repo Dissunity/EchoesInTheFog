c46fc18700b3a2c82b9babca387129b7782ce957
Marin Prusac <prusac.marin@gmail.com>
2026-05-20 19:48:04 +0200

    Time travel lever fix (#119)
    * changes to the time travel effect
    
    * time travel controller hinge interaction fix
    
    * fixes to player climbing
    
    * modified time travel effect
    
    * experimental head behaviour

--- addons/godot-xr-tools/player/player_body.gd
+++ addons/godot-xr-tools/player/player_body.gd
@@ -487,7 +487,7 @@ func slew_up(up: Vector3, slew: float) -> void:
 ## the player is in rest position
 func calibrate_player_height():
 	var base_height = camera_node.transform.origin.y + (player_head_height * XRServer.world_scale)
-	var player_height = XRToolsUserSettings.player_height * XRServer.world_scale
+	var player_height = XRToolsUserSettings.player_height * XRServer.world_scale * 0.9
 	player_height_offset = (player_height - base_height) / XRServer.world_scale
 
 
