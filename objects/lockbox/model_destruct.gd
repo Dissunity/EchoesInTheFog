1d75cc41669349905e1cdea8d408f39e791a8fb5
Charmaine <54951086+charmaine211@users.noreply.github.com>
2026-05-22 13:25:34 +0200

    128 finetune collisions and implement breaking lockbox script (#130)
    * Collision deactivated when timetraveling
    
    * Added script to lockbox to manage collision and visibility of different elements
    
    * Deactivate puzzle when it is locked
    
    * Fix the door position add collision manually to script

--- objects/lockbox/model_destruct.gd
+++ objects/lockbox/model_destruct.gd
@@ -1,9 +1,7 @@
 extends Node3D
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	for pieces: RigidBody3D in self.get_children():
 		pass # Replace with function body.
 
 
