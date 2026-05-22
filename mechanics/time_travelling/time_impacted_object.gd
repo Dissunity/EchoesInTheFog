7cfbde6444f1b5334b37a6a7678def6d250e8f6e
Marin Prusac <prusac.marin@gmail.com>
2026-05-22 19:03:23 +0200

    added time travel sound, temporarily disabled chair and table time travel

--- mechanics/time_travelling/time_impacted_object.gd
+++ mechanics/time_travelling/time_impacted_object.gd
@@ -1,4 +1,4 @@
-extends Node
+extends Node3D
 class_name TimeImpactedObject
 
 @export var present_model: Node3D
@@ -7,24 +7,25 @@ class_name TimeImpactedObject
 var present: bool = true
 
 func _ready():
-	_toggle_collisions()
+	pass
+	#_toggle_collisions()
 
 func _toggle_collisions():
 	var present_collision_shapes = present_model.find_children("*", "CollisionShape3D")
 	var past_collision_shapes = past_model.find_children("*", "CollisionShape3D")
 
-	var disable = not present
 
 	for present_shape in present_collision_shapes:
-		present_shape.disabled = not disable
+		present_shape.disabled = present
 		#print("Present shape ", present_shape.name, " disabled status: ", present_shape.disabled)
 
 	for past_shape in past_collision_shapes:
-		past_shape.disabled = disable
+		past_shape.disabled = not present
 		#print("Past shape ", past_shape.name, " disabled status: ", past_shape.disabled)
 
 
 func toggle_time():
+	return
 	present = not present
 	present_model.visible = present
 	past_model.visible = not present
