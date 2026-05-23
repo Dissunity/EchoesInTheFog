da1b68a6818812b07aa5ab6e63803f636264c4c8
Marin Prusac <prusac.marin@gmail.com>
2026-05-23 15:56:40 +0200

    changed monster distance

--- world/monster/monster.gd
+++ world/monster/monster.gd
@@ -1,5 +1,5 @@
 extends Node3D
-@export var spawn_radius: float = 6.0
+@export var spawn_radius: float = 7.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
