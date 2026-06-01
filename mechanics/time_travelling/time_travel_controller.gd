a5266d23f3b50ccb4941097e7abe0160ff005588
Marin Prusac <prusac.marin@gmail.com>
2026-06-01 14:21:19 +0200

    removed time travel light dimming

--- mechanics/time_travelling/time_travel_controller.gd
+++ mechanics/time_travelling/time_travel_controller.gd
@@ -10,7 +10,6 @@ signal time_travel_ended
 ## World environment which will be affected by time travelling
 @export var environment: WorldEnvironment
 
-@export var lights_to_dim_on_time_travel: Array[Light3D]
 
 ## All the objects which have their past and present variants which will switch upon time travel
 @export var time_impacted_objects: Array[TimeImpactedObject]
@@ -21,15 +20,12 @@ signal time_travel_ended
 ## Foghorn lever hinge which when pulled triggers the time travelling
 @export var hinge: XRToolsInteractableHinge
 
-##
 @export var puzzle_holder_collision_shape: CollisionShape3D
 
 ## Only enable this to easily test the time travelling effect
 ## When enabled, within a single press of ENTER, the time travel will happen
 @export var test: bool = false
-
 @export var lockbox: Lockbox
-
 
 @onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
 
@@ -48,7 +44,6 @@ func _time_travel():
 	time_travel_initiated.emit()
 	cooldown = 30
 	
-	
 	for obj in time_impacted_objects:
 		obj.toggle_time()
 	
@@ -59,17 +54,13 @@ func _time_travel():
 			item.visible = !item.visible
 	
 	puzzle_holder_collision_shape.disabled = !puzzle_holder_collision_shape.disabled
-
 	lockbox.is_present = !lockbox.is_present
-	
-	var old_light_values = []
+
 	var old_fog_light_energy = environment.environment.fog_light_energy
 	var old_ambient_light_sky_contribution = environment.environment.ambient_light_sky_contribution
 	
-	
 	audio_stream_player.stream = foghorn_sound
 	audio_stream_player.play(1.5)
-	#await audio_stream_player.finished
 	await get_tree().create_timer(3).timeout
 	
 	audio_stream_player.stream = lights_out_sound
@@ -77,10 +68,6 @@ func _time_travel():
 	var tween = get_tree().create_tween()
 	tween.tween_property(environment.environment, "fog_light_energy", 0, 2)
 	tween.parallel().tween_property(environment.environment, "ambient_light_sky_contribution", 0, 2)
-	for i in range(len(lights_to_dim_on_time_travel)):
-		var light = lights_to_dim_on_time_travel[i]
-		old_light_values.append(light.light_energy)
-		tween.parallel().tween_property(light, "light_energy", 0, 2)
 	tween.tween_interval(1)
 	await tween.finished
 	
@@ -94,15 +81,8 @@ func _time_travel():
 		tween.tween_property(time_travel_light, "light_energy", 500, 0.2)
 
 	tween.tween_property(time_travel_light, "light_energy", 0, 2).set_ease(Tween.EASE_OUT)
-	
-
 	tween.tween_property(environment.environment, "fog_light_energy", old_fog_light_energy, 7)
 	tween.parallel().tween_property(environment.environment, "ambient_light_sky_contribution", old_ambient_light_sky_contribution, 7)
-	
-	for i in range(len(lights_to_dim_on_time_travel)):
-		var old_value = old_light_values[i]
-		var light = lights_to_dim_on_time_travel[i]
-		tween.parallel().tween_property(light, "light_energy", old_value, 7)
 	
 	await tween.finished
 	time_travel_ended.emit()
